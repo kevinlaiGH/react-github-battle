bbf99b8aacc88dd5ea51e83d6d157f3349b83828
Kevin Lai <kevin.km.lai@gmail.com>
2019-08-14 23:53:07 +1000

    webpack configuration

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return <div>Hello Kevin!</div>;
+  }
+}
+
+// export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
