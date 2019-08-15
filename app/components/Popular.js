117fc11cc4c586d659a48d08c6d67933e736979d
Kevin Lai <kevin.km.lai@gmail.com>
2019-08-15 22:58:40 +1000

    popular.js as class component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 
 class Popular extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All"
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage });
+  }
   render() {
     const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
     return (
       <ul className="flex-center">
-        {languages.map(lang => (
-          <li key={lang}>
-            <button className="btn-clear nav-link">{lang}</button>
+        {languages.map(language => (
+          <li key={language}>
+            <button
+              className="btn-clear nav-link"
+              style={
+                language === this.state.selectedLanguage
+                  ? { color: "rgb(187, 46, 31)" }
+                  : null
+              }
+              onClick={() => this.updatedLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
     );
   }
 }
-
 export default Popular;
