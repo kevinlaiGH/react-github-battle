bfa848665e6a6b0f994a0a21ff2daca502eba700
Kevin Lai <kevin.km.lai@gmail.com>
2019-08-29 14:45:44 +1000

    refactored Tooltip

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -7,32 +7,32 @@ import {
   FaCodeBranch,
   FaExclamationTriangle
 } from "react-icons/fa";
+import Card from "./Card";
 import Loading from "./Loading";
+import Tooltip from "./Tooltip";
 
-function LanguageNav({ selectedLanguage, onUpdateLanguage }) {
+function LangaugesNav({ selected, onUpdateLanguage }) {
   const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
 
   return (
     <ul className="flex-center">
-      {languages.map((language, index) => {
-        return (
-          <li key={index}>
-            <button
-              className="btn-clear nav-link"
-              style={language === selectedLanguage ? { color: "red" } : null}
-              onClick={() => onUpdateLanguage(language)}
-            >
-              {language}
-            </button>
-          </li>
-        );
-      })}
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
     </ul>
-    /* */
   );
 }
-LanguageNav.propTypes = {
-  selectedLanguage: PropTypes.string.isRequired,
+
+LangaugesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
 };
 
@@ -45,51 +45,51 @@ function ReposGrid({ repos }) {
           owner,
           html_url,
           stargazers_count,
-          forks_count,
-          open_issues_count
+          forks,
+          open_issues
         } = repo;
         const { login, avatar_url } = owner;
 
         return (
-          <li key={html_url} className="card bg-light">
-            <h4 className="header-lg center-text">{"#" + (index + 1)}</h4>
-            <img
-              className="avatar"
-              src={avatar_url}
-              alt={`Avatar for ${login}`}
-            />
-            <h2 className="center-text">
-              <a className="link" href={html_url}>
-                {login}
-              </a>
-            </h2>
-            <ul className="card-list">
-              <li>
-                <FaUser color="rgb(255, 191, 116)" size={22} />
-                <a href={`https://github.com/${login}`}>{login}</a>
-              </li>
-              <li>
-                <FaStar color="rgb(255, 215, 0)" size={22} />
-                {stargazers_count.toLocaleString()} stars
-              </li>
-              <li>
-                <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
-                {forks_count.toLocaleString()} forks
-              </li>
-              <li>
-                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
-                {open_issues_count.toLocaleString()} open issues
-              </li>
-            </ul>
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className="card-list">
+                <li>
+                  <Tooltip text="Github username">
+                    <FaUser color="rgb(255, 191, 116)" size={22} />
+                    <a href={`https://github.com/${login}`}>{login}</a>
+                  </Tooltip>
+                </li>
+                <li>
+                  <FaStar color="rgb(255, 215, 0)" size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                  {open_issues.toLocaleString()} open
+                </li>
+              </ul>
+            </Card>
           </li>
         );
       })}
     </ul>
   );
 }
+
 ReposGrid.propTypes = {
   repos: PropTypes.array.isRequired
 };
+
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
@@ -101,12 +101,11 @@ export default class Popular extends React.Component {
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
   }
-
   componentDidMount() {
     this.updateLanguage(this.state.selectedLanguage);
   }
-
   updateLanguage(selectedLanguage) {
     this.setState({
       selectedLanguage,
@@ -123,27 +122,27 @@ export default class Popular extends React.Component {
             }
           }));
         })
-        .catch(error => {
-          console.warn("Error fetching repos", error);
+        .catch(() => {
+          console.warn("Error fetching repos: ", error);
 
           this.setState({
-            error: "There is an error fetching repos"
+            error: `There was an error fetching the repositories.`
           });
         });
     }
   }
-
   isLoading() {
     const { selectedLanguage, repos, error } = this.state;
+
     return !repos[selectedLanguage] && error === null;
   }
-
   render() {
     const { selectedLanguage, repos, error } = this.state;
+
     return (
       <React.Fragment>
-        <LanguageNav
-          selectedLanguage={selectedLanguage}
+        <LangaugesNav
+          selected={selectedLanguage}
           onUpdateLanguage={this.updateLanguage}
         />
 
